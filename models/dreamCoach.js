3b1b01eff2c1c122191a3134edad7e16724c1fa9
DeepP99 <160652318+DeepP99@users.noreply.github.com>
2024-04-04 12:59:36 -0400

    added in routes for formation page

--- models/dreamCoach.js
+++ models/dreamCoach.js
@@ -7,6 +7,8 @@ const dreamCoachSchema = new Schema({
     nationality: { type: String },
     age: { type: Number }
     // Add other fields as needed
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('DreamCoach', dreamCoachSchema);
--- models/formation.js
+++ models/formation.js
@@ -6,6 +6,8 @@ const formationSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String }
     // Add other fields as needed
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Formation', formationSchema);
