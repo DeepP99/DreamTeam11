12c067652397ba68e6c458932638d429d9e2e1da
DeepP99 <160652318+DeepP99@users.noreply.github.com>
2024-04-04 05:08:37 -0400

    added in routes for player and index

--- routes/players.js
+++ routes/players.js
@@ -1,13 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// module.exports = router;
-
 // routes/players.js
 const express = require('express');
 const router = express.Router();
